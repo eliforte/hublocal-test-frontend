13d4fae7996d96ce630f8d271cd0d52420dc016e
Elias Forte <eliasforte.dev@gmail.com>
2022-11-01 17:04:01 -0300

    refactor: turn login and register reducer into users reducer

---
+++ src/store/users/index.ts
@@ -0,0 +1,339 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+import Swal from 'sweetalert2'
+import { useParams } from 'react-router-dom'
+import { AxiosError } from 'axios'
+import api from '../../services/api'
+import { AuthConfig } from '../../services/authConfig'
+import {
+  IUserInitialState,
+  ILoginResponse,
+  IInputLogin,
+  IUser,
+  IRegisterInput,
+  IInputsEditUser
+} from './interfaces'
+
+const paramsId = useParams()
+
+const authorizationConfig = AuthConfig()
+
+const initialState: IUserInitialState = {
+  message: '',
+  statuCode: 0,
+  result: [],
+  error: '',
+  login: {
+    is_admin: false,
+    name: '',
+    token: ''
+  },
+  loading: false,
+  success: false
+}
+
+export const signUpUser = createAsyncThunk<IUser, IRegisterInput>(
+  'users/register',
+  async ({ name, password, email }, thunkApi) => {
+    try {
+      const res = await api.post('/api/v1/users', { name, email, password })
+      return res.data as IUser
+    } catch (err) {
+      let errorMessage = 'Internal Server Error'
+      if (err instanceof AxiosError) {
+        if (err.response?.data.message != null) {
+          errorMessage = err.response.data.message
+        }
+      }
+      return thunkApi.rejectWithValue(errorMessage)
+    }
+  }
+)
+
+export const signInUser = createAsyncThunk<ILoginResponse, IInputLogin>('users/login', async ({ email, password }, thunkApi) => {
+  try {
+    const res = await api.post('/api/v1/login', { email, password })
+    localStorage.setItem('user', JSON.stringify(res.data))
+    return res.data as ILoginResponse
+  } catch (err) {
+    let errorMessage = 'Internal Server Error'
+    if (err instanceof AxiosError) {
+      if (err.response?.data.message != null) {
+        errorMessage = err.response.data.message
+      }
+    }
+    return thunkApi.rejectWithValue(errorMessage)
+  }
+})
+
+export const getAllUsers = createAsyncThunk<IUser>(
+  'users/getAll',
+  async (_, thunkApi) => {
+    try {
+      const res = await api.get('/api/v1/users', { ...authorizationConfig })
+      return res.data as IUser
+    } catch (err) {
+      let errorMessage = 'Internal Server Error'
+      if (err instanceof AxiosError) {
+        if (err.response?.data.message != null) {
+          errorMessage = err.response.data.message
+        }
+      }
+      return thunkApi.rejectWithValue(errorMessage)
+    }
+  }
+)
+
+export const editUser = createAsyncThunk<IInputsEditUser, IUser>(
+  'users/edit',
+  async (editUserInfos: IInputsEditUser, thunkApi) => {
+    try {
+      const res = await api.put(`/api/v1/users/${paramsId.id}`, { ...editUserInfos }, { ...authorizationConfig })
+      return res.data as IUser
+    } catch (err) {
+      let errorMessage = 'Internal Server Error'
+      if (err instanceof AxiosError) {
+        if (err.response?.data.message != null) {
+          errorMessage = err.response.data.message
+        }
+      }
+      return thunkApi.rejectWithValue(errorMessage)
+    }
+  }
+)
+
+export const getByIdUser = createAsyncThunk<IUser>(
+  'users/getById',
+  async (_, thunkApi) => {
+    try {
+      const res = await api.get(`/api/v1/users/${paramsId.id}`, { ...authorizationConfig })
+      return res.data as IUser
+    } catch (err) {
+      let errorMessage = 'Internal Server Error'
+      if (err instanceof AxiosError) {
+        if (err.response?.data.message != null) {
+          errorMessage = err.response.data.message
+        }
+      }
+      return thunkApi.rejectWithValue(errorMessage)
+    }
+  }
+)
+
+export const deleteUser = createAsyncThunk<IUser>(
+  'users/delete',
+  async (_, thunkApi) => {
+    try {
+      const res = await api.delete(`/api/v1/users/${paramsId.id}`, { ...authorizationConfig })
+      return res.data as IUser
+    } catch (err) {
+      let errorMessage = 'Internal Server Error'
+      if (err instanceof AxiosError) {
+        if (err.response?.data.message != null) {
+          errorMessage = err.response.data.message
+        }
+      }
+      return thunkApi.rejectWithValue(errorMessage)
+    }
+  }
+)
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(signInUser.pending, (state: IUserInitialState) => {
+        state.loading = true
+      })
+      .addCase(signUpUser.pending, (state: IUserInitialState) => {
+        state.loading = true
+      })
+      .addCase(getAllUsers.pending, (state: IUserInitialState) => {
+        state.loading = true
+      })
+      .addCase(editUser.pending, (state: IUserInitialState) => {
+        state.loading = true
+      })
+      .addCase(getByIdUser.pending, (state: IUserInitialState) => {
+        state.loading = true
+      })
+      .addCase(deleteUser.pending, (state: IUserInitialState) => {
+        state.loading = true
+      })
+    builder
+      .addCase(signInUser.fulfilled, (state: IUserInitialState, action: PayloadAction<ILoginResponse>) => {
+        state.loading = false
+        state = { ...state, ...action.payload }
+        state.error = undefined
+        window.location.pathname = '/home/tickets'
+      })
+      .addCase(signUpUser.fulfilled, (state: IUserInitialState, action: PayloadAction<IUser>) => {
+        state.loading = false
+        state = { ...state, ...action.payload }
+        state.error = undefined
+        Swal.fire({
+          icon: 'success',
+          title: 'Conta criada com sucesso!',
+          text: 'Você será redirecionado para página de login.',
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+        setTimeout(() => {
+          window.location.pathname = '/'
+        }, 2000)
+      })
+      .addCase(getAllUsers.fulfilled, (state: IUserInitialState, action: PayloadAction<IUser>) => {
+        state.loading = false
+        state = { ...state, ...action.payload }
+        state.error = undefined
+      })
+      .addCase(editUser.fulfilled, (state: IUserInitialState, action: PayloadAction<IInputsEditUser>) => {
+        state.loading = false
+        state = { ...state, ...action.payload }
+        state.error = undefined
+        Swal.fire({
+          icon: 'success',
+          title: 'Usuário atualizado com sucesso!',
+          text: 'Você será redirecionado',
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+        setTimeout(() => {
+          window.location.pathname = `/home/user/details/${paramsId.id}`
+        }, 2000)
+      })
+      .addCase(deleteUser.fulfilled, (state: IUserInitialState, action: PayloadAction<IUser>) => {
+        state.loading = false
+        state.error = undefined
+        Swal.fire({
+          icon: 'success',
+          title: 'Usuário excluído com sucesso!',
+          text: 'Você será redirecionado',
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+        setTimeout(() => {
+          window.location.pathname = '/home/users'
+        }, 2000)
+      })
+    builder
+      .addCase(signInUser.rejected, (state, action: PayloadAction<unknown>) => {
+        state.loading = false
+        state.error = action.payload
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops!',
+          text: `${state.error}`,
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+      })
+      .addCase(signUpUser.rejected, (state, action: PayloadAction<unknown>) => {
+        state.loading = false
+        state.error = action.payload
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops!',
+          text: `${state.error}`,
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+      })
+      .addCase(getAllUsers.rejected, (state, action: PayloadAction<unknown>) => {
+        state.loading = false
+        state.error = action.payload
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops!',
+          text: `${state.error}`,
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+      })
+      .addCase(editUser.rejected, (state, action: PayloadAction<unknown>) => {
+        state.loading = false
+        state.error = action.payload
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops!',
+          text: `${state.error}`,
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+      })
+      .addCase(deleteUser.rejected, (state, action: PayloadAction<unknown>) => {
+        state.loading = false
+        state.error = action.payload
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops!',
+          text: `${state.error}`,
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+      })
+      .addCase(getByIdUser.rejected, (state, action: PayloadAction<unknown>) => {
+        state.loading = false
+        state.error = action.payload
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ops!',
+          text: `${state.error}`,
+          width: 400,
+          padding: '1em',
+          color: '#424242',
+          backdrop: `
+            rgba(97,97,97,0.73)
+            top
+          `
+        })
+      })
+  }
+})
+
+export default userSlice.reducer
+
+export const useUsers = (state: IUserInitialState): IUserInitialState => {
+  return state
+}
---
+++ src/store/users/interfaces.ts
@@ -0,0 +1,45 @@
+export interface IInputLogin {
+  email: string
+  password: string
+}
+
+export interface ILoginResponse {
+  token: string
+  name: string
+  is_admin: boolean
+}
+
+export interface IUserInitialState {
+  message: string
+  statuCode: number
+  result: []
+  error: string | unknown
+  login: {
+    is_admin: boolean
+    name: string
+    token: string
+  }
+  loading: boolean
+  success: boolean | undefined
+}
+
+export interface IUser {
+  name: string
+  email: string
+  is_admin: boolean
+  created_at?: Date
+  password?: string
+}
+
+export interface IRegisterInput {
+  name: string
+  email: string
+  password: string
+}
+
+export interface IInputsEditUser {
+  name?: string
+  email?: string
+  password?: string
+  is_admin?: boolean
+}
