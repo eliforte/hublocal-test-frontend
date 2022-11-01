2f0ea3c3e1736b49f4dc6c7417372490c2205b02
Elias Forte <eliasforte.dev@gmail.com>
2022-11-01 10:31:41 -0300

    feat: created new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import GlobalStyles from './styles/globalStyles'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Login } from './pages/login'
+import { Register } from './pages/register'
+import { Home } from './pages/home'
 
 export const App: React.FC = () => (
   <>
@@ -9,6 +11,8 @@ export const App: React.FC = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </>
