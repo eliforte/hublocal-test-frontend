d342e89ab8c377be5c809929f2fe7dda3d2218fc
Elias Forte <eliasforte.dev@gmail.com>
2022-11-25 16:06:33 -0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import GlobalStyles from './styles/globalStyles'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Login } from './pages/login'
 import { Register } from './pages/register'
-import { Home } from './pages/home'
 import { Companies } from './pages/companies'
+import { Places } from './pages/places'
+import { Responsibles } from './pages/responsibles'
+import { Tickets } from './pages/tickets'
+import { Users } from './pages/users'
 
 export const App: React.FC = () => (
   <>
@@ -13,8 +16,11 @@ export const App: React.FC = () => (
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home/tickets" element={<Home />} />
+        <Route path="/home/tickets" element={<Tickets />} />
         <Route path="/home/companies" element={<Companies />} />
+        <Route path="/home/places" element={<Places />} />
+        <Route path="/home/responsibles" element={<Responsibles />} />
+        <Route path="/home/users" element={<Users />} />
       </Routes>
     </BrowserRouter>
   </>
