13c4b4cad8a817617ffb4a3b705aa0ffece3613b
Elias Forte <eliasforte.dev@gmail.com>
2022-11-25 15:11:42 -0300

    style: add style in component `EmptyContent`

--- src/components/emptyContent/index.tsx
+++ src/components/emptyContent/index.tsx
@@ -10,12 +10,30 @@ export const EmptyContent: React.FC<Props> = ({ item }) => {
   const theme = useTheme()
 
   return (
-    <Box sx={{
-      maxHeigth: theme.spacing(30),
-      maxWidth: theme.spacing(30)
-    }}>
-      <Block sx={{ heigth: theme.spacing(20) }} />
-      <Typography variant="h5" gutterBottom>
+    <Box
+      display="flex"
+      alignItems="center"
+      flexDirection="column"
+      sx={{
+        backgroundColor: 'whitesmoke',
+        maxHeigth: theme.spacing(30),
+        maxWidth: '100%',
+        padding: theme.spacing(2),
+        borderRadius: '5px'
+      }}
+    >
+      <Block
+        sx={{
+          heigth: theme.spacing(20),
+          marginBottom: theme.spacing(4),
+          marginTop: theme.spacing(2)
+        }}
+      />
+      <Typography
+        variant="h5"
+        gutterBottom
+        alignSelf="center"
+      >
         {`Nenhum ${item} encontrado`}
       </Typography>
     </Box>
