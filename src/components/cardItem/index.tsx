335140f6ca69e86e8af4c09e89673e0c92409243
Elias Forte <eliasforte.dev@gmail.com>
2022-11-25 15:13:09 -0300

    style: remove unnecessary CSS

--- src/components/cardItem/index.tsx
+++ src/components/cardItem/index.tsx
@@ -16,16 +16,9 @@ export const CartItem: React.FC = () => {
       margin={theme.spacing(0)}
     >
       <Box
-        sx={{
-          backgroundColor: '#757575',
-          padding: theme.spacing(2),
-          flexDirection: 'column',
-          borderRadius: theme.spacing(2),
-          maxWidth: '90%'
-        }}
         display="flex"
-        marginLeft={theme.spacing(1)}
-        marginTop={theme.spacing(10)}
+        ml={theme.spacing(1)}
+        mt={theme.spacing(10)}
       >
         <DetailsButton id={id} path="tickets" />
         <Box>
