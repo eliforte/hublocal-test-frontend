0d698dfd685feb77f06a579853856f55b0a0a5b7
Elias Forte <eliasforte.dev@gmail.com>
2022-11-26 19:54:34 -0300

    fix: testing Loading

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,112 +9,112 @@ import {
   Container
 } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import {
-  Header,
-  Loading
-} from '../../components'
+import { Header, Loading } from '../../components'
 import { useAppDispatch } from '../../hooks'
-import { signInUser, useUsers } from '../../store/users'
+import { signInUser } from '../../store/users'
 
 export const Login: React.FC = () => {
   const [inputInfos, setInputInfos] = React.useState({
     email: '',
     password: ''
   })
+  const [loading, setLoading] = React.useState(false)
   const dispatch = useAppDispatch()
-  const user = useSelector(useUsers)
   const navigate = useNavigate()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true)
     e.preventDefault()
     dispatch(signInUser(inputInfos))
+    setLoading(false)
   }
 
   React.useEffect(() => {
     if (localStorage.getItem('user') != null) {
       navigate('/home/tickets')
     }
-  }, [user.loading])
-
-  if (user.loading) return <Loading />
+  }, [loading])
 
   return (
     <>
       <Header />
-      <Container
-        sx={{
-          margin: 'auto auto',
-          display: 'flex',
-          alignItems: 'center',
-          height: '100%'
-        }}
-        component="main"
-        maxWidth="xs"
-      >
-        <Box
+      {
+        loading
+          ? <Loading />
+          : <Container
           sx={{
-            justifyContent: 'center',
-            margin: 2,
+            margin: 'auto auto',
             display: 'flex',
-            flexDirection: 'column',
             alignItems: 'center',
-            padding: 2,
-            backgroundColor: '#f5f5f5',
-            borderRadius: '5px'
+            height: '100%'
           }}
+          component="main"
+          maxWidth="xs"
         >
-          <Typography color="#212121" component="h1" variant="h5">
-            Entrar
-          </Typography>
-          <Box component="form" onSubmit={(e) => handleSubmit(e)} sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              onChange={ (e) => setInputInfos({ ...inputInfos, email: e.target.value })}
-              name="email"
-              autoFocus
-              sx={{ backgroundColor: '#eeeeee', borderRadius: 1 }}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              onChange={ (e) => setInputInfos({ ...inputInfos, password: e.target.value })}
-              type="password"
-              id="password"
-              sx={{ backgroundColor: '#eeeeee', borderRadius: 1 }}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                backgroundColor: '#424242',
-                '&:hover': {
-                  backgroundColor: '#212121'
-                }
-              }}
-            >
+          <Box
+            sx={{
+              justifyContent: 'center',
+              margin: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              padding: 2,
+              backgroundColor: '#f5f5f5',
+              borderRadius: '5px'
+            }}
+          >
+            <Typography color="#212121" component="h1" variant="h5">
               Entrar
-            </Button>
-            <Grid container sx={{ justifyContent: 'center' }}>
-              <Grid item>
-                <Link component="button" onClick={() => navigate('/register') } variant="body2">
-                  {'Ainda não tem conta? Cadastre-se'}
-                </Link>
+            </Typography>
+            <Box component="form" onSubmit={(e) => handleSubmit(e)} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                onChange={ (e) => setInputInfos({ ...inputInfos, email: e.target.value })}
+                name="email"
+                autoFocus
+                sx={{ backgroundColor: '#eeeeee', borderRadius: 1 }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                onChange={ (e) => setInputInfos({ ...inputInfos, password: e.target.value })}
+                type="password"
+                id="password"
+                sx={{ backgroundColor: '#eeeeee', borderRadius: 1 }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  backgroundColor: '#424242',
+                  '&:hover': {
+                    backgroundColor: '#212121'
+                  }
+                }}
+              >
+                Entrar
+              </Button>
+              <Grid container sx={{ justifyContent: 'center' }}>
+                <Grid item>
+                  <Link component="button" onClick={() => navigate('/register') } variant="body2">
+                    {'Ainda não tem conta? Cadastre-se'}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      }
     </>
   )
 }
