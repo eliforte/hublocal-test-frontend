c4a5ee82d314fd233cd5032e8466b5c947257ffd
Elias Forte <eliasforte.dev@gmail.com>
2022-11-28 02:49:24 -0300

    feat: created page `PlaceCreate`

---
+++ src/pages/placeCreate/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { Box, useTheme } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { DrawerMenu, Header, Create } from '../../components'
+
+export const PlaceCreate: React.FC = () => {
+  const theme = useTheme()
+  const navigate = useNavigate()
+
+  React.useEffect(() => {
+    if (localStorage.getItem('user') == null) {
+      navigate('/')
+    }
+  }, [])
+
+  return (
+    <Box pb={theme.spacing(4)} width="100%">
+      <Box
+        display="flex"
+        flexDirection="row"
+      >
+        <DrawerMenu />
+        <Header />
+      </Box>
+      <Box>
+        <Create title="Registrar novo estabelecimento"/>
+      </Box>
+    </Box>
+  )
+}
