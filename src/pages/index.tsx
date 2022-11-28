d1d569e1955381d5e8c40983ab6f2cd1fe272952
Elias Forte <eliasforte.dev@gmail.com>
2022-11-28 02:53:42 -0300

    feat: add all pages into unique file for export

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import { Companies } from './companies'
+import { Places } from './places'
+import { Users } from './users'
+import { Login } from './login'
+import { Register } from './register'
+import { Responsibles } from './responsibles'
+import { Tickets } from './tickets'
+import { UserCreate } from './userCreate'
+import { UserDetails } from './userDetails'
+import { CompanyCreate } from './companyCreate'
+import { CompanyDetails } from './companyDetails'
+import { PlaceDetails } from './placeDetails'
+import { PlaceCreate } from './placeCreate'
+import { ResponsibleDetails } from './responsibleDetails'
+import { ResponsibleCreate } from './responsibleCreate'
+import { TicketDetails } from './ticketDetails'
+import { TicketCreate } from './ticketCreate'
+
+export {
+  Companies,
+  Login,
+  Places,
+  Register,
+  Responsibles,
+  Tickets,
+  UserCreate,
+  UserDetails,
+  Users,
+  CompanyCreate,
+  ResponsibleDetails,
+  CompanyDetails,
+  PlaceDetails,
+  PlaceCreate,
+  ResponsibleCreate,
+  TicketDetails,
+  TicketCreate
+}
