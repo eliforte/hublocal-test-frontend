1bbdffce372479bf48f869b512418967534e1d78
Elias Forte <eliasforte.dev@gmail.com>
2022-11-28 02:49:55 -0300

    feat: created page `PlaceDetails`

---
+++ src/pages/placeDetails/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { Box, useTheme } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { DrawerMenu, Header, Details } from '../../components'
+
+export const PlaceDetails: React.FC = () => {
+  const navigate = useNavigate()
+  const theme = useTheme()
+
+  React.useEffect(() => {
+    if (localStorage.getItem('user') == null) {
+      navigate('/')
+    }
+  }, [])
+
+  return (
+    <Box pb={theme.spacing(4)} width="100%">
+      <Box
+        display="flex"
+        flexDirection="row"
+      >
+        <DrawerMenu />
+        <Header />
+      </Box>
+      <Box>
+        <Details title="Detalhes do estabelecimento"/>
+      </Box>
+    </Box>
+  )
+}
